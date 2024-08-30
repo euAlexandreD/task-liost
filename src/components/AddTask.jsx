38f033324df883bf240a000a26323ba14a3be0d6
Alexandre Portella <eualexandre.dev@gmail.com>
2024-08-30 03:15:40 -0300

    feat: add react-alert setup

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,30 @@
-import CustomInput from "./CustomInput";
-import "./AddTask.scss";
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { useAlert } from "react-alert";
+import axios from "axios";
+
+import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
+
+import "./AddTask.scss";
 
 const AddTask = () => {
     const [task, setTask] = useState("");
 
+    const alert = useAlert();
+
     const onChange = (e) => {
         setTask(e.target.value);
+    };
+
+    const hendleTaskAdittion = async () => {
+        try {
+            if (task.length === 0) {
+                return alert.error(
+                    "A tarefa precisa de uma descrição para ser adicionada"
+                );
+            }
+        } catch (error) {}
     };
 
     return (
@@ -17,6 +35,9 @@ const AddTask = () => {
                     value={task}
                     onChange={onChange}
                 />
+                <CustomButton onClick={hendleTaskAdittion}>
+                    <FaPlus size={14} color="#ffffff" />
+                </CustomButton>
             </div>
         </>
     );
