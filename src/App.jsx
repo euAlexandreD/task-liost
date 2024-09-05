d315b3e812ba5d7677e75f9a8837eeb5939ade54
Alexandre Portella da Silva <eualexandre.dev@gmail.com>
2024-09-05 16:12:30 -0300

    testando

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         <>
             <div className="app-container">
                 <SideBar />
-                <Tasks />
+                <Tasks/>
             </div>
         </>
     );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -19,6 +19,9 @@ const Tasks = () => {
         }
     };
 
+
+  
+
     useEffect(() => {
         fetchTasks();
     }, []);
